e4ecc0b1325bed8955cb0ae07df8aed9b9705a37
strongQ <zhangqi1270187870@hotmail.com>
2017-07-20 21:04:38 +0800

    api

---
+++ EF.Core/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Core
+{
+    public class Project:BaseEntity
+    {
+        /// <summary>
+        /// 项目名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 导航属性--Module
+        /// </summary>
+        public virtual ICollection<Module> Modules { get; set; }
+    }
+}
